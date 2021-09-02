50d09ac2751c7da46f833622b495feb0323f22d2
zhiruili <zhiruili@tencent.com>
2021-09-02 15:52:14 +0800

    update

--- AndroidSample/mod/src/main/java/com/example/mod/MainActivity.java
+++ AndroidSample/mod/src/main/java/com/example/mod/MainActivity.java
@@ -1,20 +1,13 @@
 package com.example.mod;
 
-import android.os.Bundle;
 import android.widget.Toast;
 
 import com.unity3d.player.UnityPlayer;
 import com.unity3d.player.UnityPlayerActivity;
 
 public class MainActivity extends UnityPlayerActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
     public void showMessage(final String message) {
         runOnUiThread(() -> Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show());
         UnityPlayer.UnitySendMessage("Canvas", "ChangeText", "Hello from Android!");
     }
-
 }
